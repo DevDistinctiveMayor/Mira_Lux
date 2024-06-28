fb267125dade02af65689acd30d21b80f00ada53
Ekundayo Mayowa T. <ekundayomayor@gmail.com>
2024-06-28 15:52:09 +0100

    server_api

--- src/components/js/Women/Women.js
+++ src/components/js/Women/Women.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import "./WomenClothing.css";
 import { IoStarSharp } from "react-icons/io5";
 import Like from "../Like";
+
 const WomenClothing = () => {
   const [clothingData, setClothingData] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -12,7 +13,7 @@ const WomenClothing = () => {
     const fetchClothingData = async () => {
       try {
         const response = await axios.get(
-          "http://localhost:5000/api/women-clothing"
+          "https://mira-boutique-backend-1.onrender.com/api/women-clothing"
         );
         setClothingData(response.data);
       } catch (err) {
@@ -34,35 +35,32 @@ const WomenClothing = () => {
   }
 
   return (
-    <div class="flex justify-center bg-slate-100 -mb-[6rem]">
+    <div className="flex justify-center bg-slate-100 -mb-[6rem]">
       <div className="mt-[5rem]">
-        <h1 class="flex justify-center font-bold">Women's Clothing</h1>
+        <h1 className="flex justify-center font-bold">Women's Clothing</h1>
         <div className="clothing-grid">
           {clothingData.map((item) => (
             <div key={item.id} className="clothing-item">
               {item.image && (
                 <img
-                  src={`http://localhost:5000${item.image}`}
+                  src={`https://mira-boutique-backend-1.onrender.com${item.image}`}
                   alt={item.title}
                   width="100"
                   className="w-max"
                 />
               )}
-              <div class="mt-[30px]">
+              <div className="mt-[30px]">
                 <h2>{item.title}</h2>
-                <p className="">{item.description}</p>
-                <p className="font-medium">Quantity : {item.Quantity}</p>
-                <div class="flex justify-between">
-                  <p class="text-blue-600 font-medium">₦{item.price}</p>
-                 
-                  <Like class="mt-3 " />
+                <p>{item.description}</p>
+                <p className="font-medium">Quantity: {item.Quantity}</p>
+                <div className="flex justify-between">
+                  <p className="text-blue-600 font-medium">₦{item.price}</p>
+                  <Like className="mt-3" />
                 </div>
-                <div class="flex gap-1 mt-1">
-                    <IoStarSharp class="mt-1 text-yellow-400" fontSize={18} />
-                    <span class="text-slate-500 font-medium">
-                      {item.rating}
-                    </span>
-                  </div>
+                <div className="flex gap-1 mt-1">
+                  <IoStarSharp className="mt-1 text-yellow-400" fontSize={18} />
+                  <span className="text-slate-500 font-medium">{item.rating}</span>
+                </div>
               </div>
             </div>
           ))}
